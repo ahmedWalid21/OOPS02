d476517ac74387adf4bc74eee044530ce88e6427
Y.C.C <Y.C.C@DESKTOP-9Q2CJID>
2025-07-23 15:53:00 +0300

    inheritance

---
+++ OOPS02/Inheritance/Parent.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS02.Inheritance
+{
+    internal class Parent
+    {
+        public int x {  get; set; }
+        public int y { get; set; }
+
+
+
+
+        public void fun1()
+        {
+            Console.WriteLine("i'm parent [base]");
+        }
+        public void fun2()
+        {
+            Console.WriteLine($"X: {x}, Y: {y}");
+        }
+        public Parent(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"X: {x}  , Y: {y}"; 
+        }
+
+
+
+
+
+
+
+
+    }
+}
--- OOPS02/Program.cs
+++ OOPS02/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace OOPS02
+﻿using OOPS02.Inheritance;
+
+namespace OOPS02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            #region Inheritance
+            // Parent p = new Parent(1, 2);
+            // Console.WriteLine(p);
+            // p.fun1();
+            // p.fun2();
+
+            //Child c = new Child(1, 2, 3);
+            //Console.WriteLine(c);
+            //c.fun1();
+            //c.fun2();
+
+
+            #endregion
+
+
+
         }
     }
 }
